9f2837b58576a66b7321264fce7a40d6a2961d2d
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-04-25 00:32:18 -0400

    Simplified legacy script

--- Legacy/1.make_dust_files.py
+++ Legacy/1.make_dust_files.py
@@ -8,13 +8,11 @@ mpl.use('Agg')
 
 np.seterr(all='ignore')
 
-from hyperion.util.functions import filename2hdf5
-from hyperion.dust import SimpleSphericalDust, prepare_emiss
+from hyperion.dust import SimpleSphericalDust
 
 for dustfile in glob.glob(os.path.join('input/', '*')):
     print "Processing %s" % dustfile
-    dustfile_out = filename2hdf5(dustfile)
     d = SimpleSphericalDust(dustfile)
     d.optical_properties._extrapolate(1.e-3, 1.e7)
-    d.write('dust_files/' + os.path.basename(dustfile_out))
-    d.plot('dust_files/' + os.path.basename(dustfile_out).replace('.hdf5', '.png'))
+    d.write('dust_files/' + os.path.basename(dustfile).replace('.par', '.hdf5'))
+    d.plot('dust_files/' + os.path.basename(dustfile).replace('.par', '.png'))
