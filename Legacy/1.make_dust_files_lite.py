5203914dc5ffd3d0e597f691a986074d6d53f743
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-04-19 12:03:59 +0200

    Updated scripts to make Legacy dust files for Hyperion 0.8.5

--- Legacy/1.make_dust_files_lite.py
+++ Legacy/1.make_dust_files_lite.py
@@ -14,5 +14,6 @@ for dustfile in glob.glob(os.path.join('input/', '*')):
     print "Processing %s" % dustfile
     d = TTsreDust(dustfile)
     d.optical_properties._extrapolate(1.e-3, 1.e7)
-    d.write('dust_files/' + os.path.basename(dustfile).replace('.par', '.hdf5'))
-    d.plot('dust_files/' + os.path.basename(dustfile).replace('.par', '.png'))
+    d.emissivities.set_lte(d.optical_properties, n_temp=100, temp_min=0.1, temp_max=1600.)
+    d.write('dust_files_lite/' + os.path.basename(dustfile).replace('.par', '_lite.hdf5'))
+    d.plot('dust_files_lite/' + os.path.basename(dustfile).replace('.par', '_lite.png'))
